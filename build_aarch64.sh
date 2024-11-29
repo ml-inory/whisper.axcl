c6b3f2786640a8bc6619b9dd38834af75af75ba5
rzyang <584688538@qq.com>
2024-11-30 01:32:00 +0800

    修改opencc为源码编译

--- build_aarch64.sh
+++ build_aarch64.sh
@@ -1,5 +1,5 @@
-mkdir -p build
-cd build
+mkdir -p build_aarch64
+cd build_aarch64
 cmake -DCMAKE_TOOLCHAIN_FILE=../toolchain/aarch64-none-linux-gnu.toolchain.cmake -DCMAKE_INSTALL_PREFIX=../install -DCMAKE_BUILD_TYPE=Release ..
 make -j8
 make install
